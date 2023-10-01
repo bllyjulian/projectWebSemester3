755fc94697349bec80caefd8f6e5240c8ff297cd
bllyjulian <billyjulian75@gmail.com>
2023-10-01 13:27:29 +0700

    coba API

--- admin/crudphp/api.php
+++ admin/crudphp/api.php
@@ -1,15 +1,12 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "", "codingcamp");
 
-// Cek koneksi
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 
-// Mengatur header untuk respons JSON
 header("Content-Type: application/json");
 
-// Mengambil data dari database (contoh)
 $sql = "SELECT * FROM tb_akun";
 $result = mysqli_query($conn, $sql);
 
@@ -18,9 +15,9 @@ while ($row = mysqli_fetch_assoc($result)) {
     $data[] = $row;
 }
 
-// Mengembalikan data sebagai respons JSON
+// var_dump($data); // Ini akan mencetak data untuk memeriksa apakah data ada
+
 echo json_encode($data);
 
-// Menutup koneksi database
 mysqli_close($conn);
 ?>
---
+++ api/data.php
@@ -0,0 +1,45 @@
+<?php
+  require_once('connection.php');
+  
+  if(empty($_GET)){
+    $query = mysqli_query($connection, "SELECT * FROM tb_akun");
+  
+    $result = array();
+    while($row = mysqli_fetch_assoc($query)){
+      $result[] = array(
+        'username' => $row['username'],
+        'nama_lengkap' => $row['nama_lengkap'],
+        'password' => $row['password'],
+        'foto_profil' => base64_encode($row['foto_profil']),
+        'no_hp' => $row['no_hp'],
+        'email' => $row['email'],
+        'status' => $row['status'],
+        'id_lvl' => $row['id_lvl']
+      );
+    }
+    echo json_encode(array('result' => $result));
+  } else {
+    // Note: Always use prepared statements for security.
+    $username = mysqli_real_escape_string($connection, $_GET['username']);
+    $query = mysqli_query($connection, "SELECT * FROM tb_akun WHERE username='$username'");
+    
+    $result = array();
+    while($row = mysqli_fetch_assoc($query)){
+      $result[] = array(
+        'username' => $row['username'],
+        'nama_lengkap' => $row['nama_lengkap'],
+        'password' => $row['password'],
+        'foto_profil' => base64_encode($row['foto_profil']),
+        'no_hp' => $row['no_hp'],
+        'email' => $row['email'],
+        'status' => $row['status'],
+        'id_lvl' => $row['id_lvl']
+      );
+    }
+    echo json_encode($result);
+  }
+  
+  mysqli_close($connection);
+
+  
+?>
