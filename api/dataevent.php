e475a633f1aa1570e30aac4079086b63298e531e
bllyjulian <billyjulian75@gmail.com>
2023-10-16 15:42:20 +0700

    api event

---
+++ api/dataevent.php
@@ -0,0 +1,47 @@
+<?php
+  require_once('connection.php');
+  
+  if(empty($_GET)){
+    $query = mysqli_query($connection, "SELECT * FROM tb_event");
+  
+    $result = array();
+    while($row = mysqli_fetch_assoc($query)){
+      $result[] = array(
+        'id_event' => $row['id_event'],
+        'judul_event' => $row['judul_event'],
+        'kuota' => $row['kuota'],
+        'pelaksanaan' => $row['pelaksanaan'],
+        'keterangan' => $row['keterangan'],
+        'lokasi' => $row['lokasi'],
+        'link_pendaftaran' => $row['link_pendaftaran'],
+        'gambar' => $row['gambar'],
+        'tanggal' => $row['tanggal']
+      );
+    }
+    echo json_encode($result);
+  } else {
+    // Note: Always use prepared statements for security.
+    $id = mysqli_real_escape_string($connection, $_GET['id_event']);
+    $query = mysqli_query($connection, "SELECT * FROM tb_event WHERE id_event='$id'");
+    
+    $result = array();
+    while($row = mysqli_fetch_assoc($query)){
+      $result[] = array(
+        'id_event' => $row['id_event'],
+        'judul_event' => $row['judul_event'],
+        'kuota' => $row['kuota'],
+        'pelaksanaan' => $row['pelaksanaan'],
+        'keterangan' => $row['keterangan'],
+        'lokasi' => $row['lokasi'],
+        'link_pendaftaran' => $row['link_pendaftaran'],
+        'gambar' => $row['gambar'],
+        'tanggal' => $row['tanggal']
+      );
+    }
+    echo json_encode($result);
+  }
+  
+  mysqli_close($connection);
+
+  
+?>
