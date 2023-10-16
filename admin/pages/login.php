842ba61bebc8fbe424b29103e4f79a707420d8c8
bllyjulian <billyjulian75@gmail.com>
2023-10-16 10:40:58 +0700

    tes api foto profil

--- admin/pages/login.php
+++ admin/pages/login.php
@@ -89,8 +89,8 @@
         <div class="container">
           <div class="row justify-content-center">
             <div class="col-lg-5 text-center mx-auto">
-              <h1 class="text-white mb-2 mt-5">Welcome!</h1>
-              <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for free.</p>
+              <h1 class="text-white mb-2 mt-5">Selamat Datang</h1>
+              <p class="text-lead text-white">                    Silahkan masukkan username dan password anda!</p>
             </div>
           </div>
         </div>
@@ -99,11 +99,11 @@
         <div class="row mt-lg-n10 mt-md-n11 mt-n10">
           <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
             <div class="card z-index-0">
-              <div class="card-header text-center pt-4">
+              <!-- <div class="card-header text-center pt-4">
                 <h5>Register with</h5>
-              </div>
+              </div> -->
               <div class="row px-xl-5 px-sm-4 px-3">
-                <div class="col-3 ms-auto px-1">
+                <!-- <div class="col-3 ms-auto px-1">
                   <a class="btn btn-outline-light w-100" href="javascript:;">
                     <svg width="24px" height="32px" viewBox="0 0 64 64" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink32">
                       <g id="Artboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -114,8 +114,8 @@
                       </g>
                     </svg>
                   </a>
-                </div>
-                <div class="col-3 px-1">
+                </div> -->
+                <!-- <div class="col-3 px-1">
                   <a class="btn btn-outline-light w-100" href="javascript:;">
                     <svg width="24px" height="32px" viewBox="0 0 64 64" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                       <g id="Artboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -125,8 +125,8 @@
                       </g>
                     </svg>
                   </a>
-                </div>
-                <div class="col-3 me-auto px-1">
+                </div> -->
+                <!-- <div class="col-3 me-auto px-1">
                   <a class="btn btn-outline-light w-100" href="javascript:;">
                     <svg width="24px" height="32px" viewBox="0 0 64 64" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                       <g id="Artboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -139,10 +139,10 @@
                       </g>
                     </svg>
                   </a>
-                </div>
+                </div> -->
                 <div class="mt-2 position-relative text-center">
                   <p class="text-sm font-weight-bold mb-2 text-secondary text-border d-inline z-index-2 bg-white px-3">
-                    or
+                    Login
                   </p>
                 </div>
               </div>
@@ -157,13 +157,13 @@
                   <div class="form-check form-check-info text-left">
                     <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
                     <label class="form-check-label" for="flexCheckDefault">
-                      I agree the <a href="javascript:;" class="text-dark font-weight-bolder">Terms and Conditions</a>
+                      Ingat saya <a href="javascript:;" class="text-dark font-weight-bolder"></a>
                     </label>
                   </div>
                   <div class="text-center">
                     <button type="submit" class="btn bg-gradient-dark w-100 my-4 mb-2" name="proses_login" >Login</button>
                   </div>
-                  <p class="text-sm mt-3 mb-0">Already have an account? <a href="javascript:;" class="text-dark font-weight-bolder">Sign in</a></p>
+                  <!-- <p class="text-sm mt-3 mb-0">Already have an account? <a href="javascript:;" class="text-dark font-weight-bolder">Sign in</a></p> -->
                 </form>
               </div>
             </div>
