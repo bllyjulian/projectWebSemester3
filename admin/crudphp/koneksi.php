fb71a83cc343666a48a52dfff328a41278e771ed
bllyjulian <billyjulian75@gmail.com>
2023-11-04 20:41:07 +0700

    benerin koneksi localhost/hosting

--- admin/crudphp/koneksi.php
+++ admin/crudphp/koneksi.php
@@ -1,9 +1,15 @@
 <?php
-    // konfigurasi db
+    // koneksi di localhost
     $host = 'localhost';
     $user = 'root';
     $pass = '';
     $db   = 'codingcamp1';
+
+    //koneksi di hosting
+    // $host = 'localhost';
+    // $user = 'codingc1_bllly';
+    // $pass = 'Soekamti@08';
+    // $db   = 'codingc1_camp';
     try {
         $koneksi = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
         $koneksi->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -11,5 +17,7 @@
     catch (PDOException $e) {
         return 'Connection failed: ' . $e->getMessage();
     }
+
+
 
 ?>
--- api/connection.php
+++ api/connection.php
@@ -1,12 +1,13 @@
 <?php
+//koneksi di localhost
   define('HOST', 'localhost');
   define('USER', 'root');
   define('PASS', '');
   define('DB', 'codingcamp1');
-
+//koneksi di hosting
+  // define('HOST', 'localhost');
+  // define('USER', 'codingc1_bllly');
+  // define('PASS', 'Soekamti@08');
+  // define('DB', 'codingc1_camp');
   $connection = mysqli_connect(HOST, USER, PASS, DB) or die('Unable connect');
-
-
-
-
 ?>
