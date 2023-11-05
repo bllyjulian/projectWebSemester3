78b63e491fdea90014c15f6fab25728ecf3478b9
bllyjulian <billyjulian75@gmail.com>
2023-11-05 18:38:52 +0700

    koneksi API

--- api/connection.php
+++ api/connection.php
@@ -1,32 +1,33 @@
 <?php
-$billy = "local";
+
+$billy = "local"; // Ganti dengan "hosting" jika Anda ingin menggunakan koneksi hosting
 
 try {
     if ($billy === "local") {
-        define('HOST', 'localhost');
-        define('USER', 'root');
-        define('PASS', '');
-        define('DB', 'codingcamp1');
+      $host = 'localhost';
+      $user = 'codingc1_bllly';
+      $pass = 'Soekamti@08';
+      $db   = 'codingc1_camp';
     } else {
-        define('HOST', 'localhost');
-        define('USER', 'codingc1_bllly');
-        define('PASS', 'Soekamti@08');
-        define('DB', 'codingc1_camp');
+      $host = 'localhost';
+      $user = 'root';
+      $pass = '';
+      $db   = 'codingcamp1';
     }
 
-    $connection = mysqli_connect(HOST, USER, PASS, DB);
+    $connection = mysqli_connect($host, $user, $pass, $db);
 
     if (!$connection) {
         throw new Exception("Unable to connect to the database: " . mysqli_connect_error());
     }
 } catch (Exception $e) {
     // Jika koneksi pertama gagal, coba koneksi kedua
-    define('HOST', 'localhost');
-    define('USER', 'codingc1_bllly');
-    define('PASS', 'Soekamti@08');
-    define('DB', 'codingc1_camp');
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db   = 'codingcamp1';
 
-    $connection = mysqli_connect(HOST, USER, PASS, DB);
+    $connection = mysqli_connect($host, $user, $pass, $db);
 
     if (!$connection) {
         die('Unable to connect to the database: ' . mysqli_connect_error());
