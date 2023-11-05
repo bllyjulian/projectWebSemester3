d62a7b267bcddeb2d75e85fc71febee9dc0828de
bllyjulian <billyjulian75@gmail.com>
2023-11-05 18:23:25 +0700

    benerin koneksi

--- admin/crudphp/koneksi.php
+++ admin/crudphp/koneksi.php
@@ -1,23 +1,25 @@
 <?php
-    // koneksi di localhost
-    $host = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $db   = 'codingcamp1';
+    $billy = "local";
 
-    //koneksi di hosting
-    // $host = 'localhost';
-    // $user = 'codingc1_bllly';
-    // $pass = 'Soekamti@08';
-    // $db   = 'codingc1_camp';
+    if ($billy === "local") {
+        $host = 'localhost';
+        $user = 'root';
+        $pass = '';
+        $db   = 'codingcamp1';
+    } else {
+        $host = 'localhost';
+        $user = 'codingc1_bllly';
+        $pass = 'Soekamti@08';
+        $db   = 'codingc1_camp';
+    }
+    
     try {
         $koneksi = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
         $koneksi->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
     }
-    catch (PDOException $e) {
-        return 'Connection failed: ' . $e->getMessage();
-    }
-
+    
 
 
 ?>
--- api/connection.php
+++ api/connection.php
@@ -1,13 +1,18 @@
 <?php
-//koneksi di localhost
+
+  $billy = "local";
+
+  if ($billy === "local") {
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASS', '');
+    define('DB', 'codingcamp1');
+  } else {
   define('HOST', 'localhost');
-  define('USER', 'root');
-  define('PASS', '');
-  define('DB', 'codingcamp1');
-//koneksi di hosting
-  // define('HOST', 'localhost');
-  // define('USER', 'codingc1_bllly');
-  // define('PASS', 'Soekamti@08');
-  // define('DB', 'codingc1_camp');
+  define('USER', 'codingc1_bllly');
+  define('PASS', 'Soekamti@08');
+  define('DB', 'codingc1_camp');
+  }
+  
   $connection = mysqli_connect(HOST, USER, PASS, DB) or die('Unable connect');
 ?>
