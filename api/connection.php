a8f3d5b9095ca18a80a28a4fea6c50e475950c9b
bllyjulian <billyjulian75@gmail.com>
2023-11-05 18:33:48 +0700

    fix bug koneksi API

--- api/connection.php
+++ api/connection.php
@@ -1,18 +1,36 @@
 <?php
+$billy = "local";
 
-  $billy = "local";
+try {
+    if ($billy === "local") {
+        define('HOST', 'localhost');
+        define('USER', 'root');
+        define('PASS', '');
+        define('DB', 'codingcamp1');
+    } else {
+        define('HOST', 'localhost');
+        define('USER', 'codingc1_bllly');
+        define('PASS', 'Soekamti@08');
+        define('DB', 'codingc1_camp');
+    }
 
-  if ($billy === "local") {
+    $connection = mysqli_connect(HOST, USER, PASS, DB);
+
+    if (!$connection) {
+        throw new Exception("Unable to connect to the database: " . mysqli_connect_error());
+    }
+} catch (Exception $e) {
+    // Jika koneksi pertama gagal, coba koneksi kedua
     define('HOST', 'localhost');
     define('USER', 'root');
     define('PASS', '');
     define('DB', 'codingcamp1');
-  } else {
-  define('HOST', 'localhost');
-  define('USER', 'codingc1_bllly');
-  define('PASS', 'Soekamti@08');
-  define('DB', 'codingc1_camp');
-  }
-  
-  $connection = mysqli_connect(HOST, USER, PASS, DB) or die('Unable connect');
+
+    $connection = mysqli_connect(HOST, USER, PASS, DB);
+
+    if (!$connection) {
+        die('Unable to connect to the database: ' . mysqli_connect_error());
+    }
+}
+
 ?>
