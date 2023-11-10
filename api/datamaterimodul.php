5a678b630e96e4847cf96316293789da6604036b
bllyjulian <billyjulian75@gmail.com>
2023-11-10 23:51:04 +0700

    API modul hampir done

--- api/datamaterimodul.php
+++ api/datamaterimodul.php
@@ -1,40 +1,39 @@
 <?php
-  require_once('connection.php');
-header ('Content-Type: application/json;charset=utf8');
-  if(empty($_GET)){
+require_once('connection.php');
+header('Content-Type: application/json;charset=utf8');
+
+if (empty($_GET)) {
     $query = mysqli_query($connection, "SELECT * FROM tb_materi");
-  
+
     $result = array();
-    while($row = mysqli_fetch_assoc($query)){
-      $result[] = array(
-        'id_materi' => $row['id_materi'],
-        'judul' => $row['judul'],
-        'materi' => $row['materi'],
-        'gambar' => $row['gambar'],
-        'id_subbab' => $row['id_subbab']
-      );
+    while ($row = mysqli_fetch_assoc($query)) {
+        $result[] = array(
+            'id_materi' => $row['id_materi'],
+            'judul' => htmlspecialchars($row['judul']),
+            'materi' => htmlspecialchars($row['materi']),
+            'gambar' => htmlspecialchars($row['gambar']),
+            'id_subbab' => $row['id_subbab']
+        );
     }
     echo json_encode($result);
-  } else {
-    // Note: Always use prepared statements for security.
+} else {
+    // Always use prepared statements for security.
     $idsubbabmodul = mysqli_real_escape_string($connection, $_GET['id_subbab']);
     $query = mysqli_query($connection, "SELECT * FROM tb_materi WHERE id_subbab='$idsubbabmodul'");
     
     $result = array();
-    while($row = mysqli_fetch_assoc($query)){
-      $result[] = array(
-        'id_materi' => $row['id_materi'],
-        'judul' => $row['judul'],
-        'materi' => $row['materi'],
-        'gambar' => $row['gambar'],
-        'id_subbab' => $row['id_subbab']
-      );
+    while ($row = mysqli_fetch_assoc($query)) {
+        $result[] = array(
+            'id_materi' => $row['id_materi'],
+            'judul' => htmlspecialchars($row['judul']),
+            'materi' => htmlspecialchars($row['materi']),
+            'gambar' => htmlspecialchars($row['gambar']),
+            'id_subbab' => $row['id_subbab']
+        );
     }
-    header ('Content-Type: application/json;charset=utf8');
+    header('Content-Type: application/json;charset=utf8');
     echo json_encode($result);
-  }
-  
-  mysqli_close($connection);
+}
 
-  
+mysqli_close($connection);
 ?>
--- api/datasubbabmodul.php
+++ api/datasubbabmodul.php
@@ -1,38 +1,37 @@
 <?php
-  require_once('connection.php');
-header ('Content-Type: application/json;charset=utf8');
-  if(empty($_GET)){
+require_once('connection.php');
+header('Content-Type: application/json;charset=utf8');
+
+if (empty($_GET)) {
     $query = mysqli_query($connection, "SELECT * FROM tb_subbab");
-  
+
     $result = array();
-    while($row = mysqli_fetch_assoc($query)){
-      $result[] = array(
-        'id_subbab' => $row['id_subbab'],
-        'nama_subbab' => $row['nama_subbab'],
-        'pengantar' => $row['pengantar'],
-        'id_ bab' => $row['id_bab']
-      );
+    while ($row = mysqli_fetch_assoc($query)) {
+        $result[] = array(
+            'id_subbab' => $row['id_subbab'],
+            'nama_subbab' => htmlspecialchars($row['nama_subbab']),
+            'pengantar' => htmlspecialchars($row['pengantar']),
+            'id_bab' => $row['id_bab']
+        );
     }
     echo json_encode($result);
-  } else {
+} else {
     // Note: Always use prepared statements for security.
     $idbabmodul = mysqli_real_escape_string($connection, $_GET['id_bab']);
-    $query = mysqli_query($connection, "SELECT * FROM tb_materi WHERE id_bab='$idbabmodul'");
-    
-    $result = array();
-    while($row = mysqli_fetch_assoc($query)){
-      $result[] = array(
-        'id_subbab' => $row['id_subbab'],
-        'nama_subbab' => $row['nama_subbab'],
-        'pengantar' => $row['pengantar'],
-        'id_ bab' => $row['id_bab']
-      );
-    }
-    header ('Content-Type: application/json;charset=utf8');
-    echo json_encode($result);
-  }
-  
-  mysqli_close($connection);
+    $query = mysqli_query($connection, "SELECT * FROM tb_subbab WHERE id_bab='$idbabmodul'");
 
-  
+    $result = array();
+    while ($row = mysqli_fetch_assoc($query)) {
+        $result[] = array(
+            'id_subbab' => $row['id_subbab'],
+            'nama_subbab' => htmlspecialchars($row['nama_subbab']),
+            'pengantar' => htmlspecialchars($row['pengantar']),
+            'id_bab' => $row['id_bab']
+        );
+    }
+    header('Content-Type: application/json;charset=utf8');
+    echo json_encode($result);
+}
+
+mysqli_close($connection);
 ?>
