84a7d69e2dbbd52f724f9a5ace4900bae65b2351
bllyjulian <billyjulian75@gmail.com>
2023-11-23 02:10:35 +0700

    FIX bug login session

--- admin/crudphp/tambahmodul.php
+++ admin/crudphp/tambahmodul.php
@@ -17,7 +17,6 @@ if (!isset($_SESSION['USER_INFO'])) {
 }
 $userInfo = $_SESSION['USER_INFO'];
 ?>
-  <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
   <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
