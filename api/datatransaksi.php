73c17afb9e441983b081edb7b1229c615e19d9d7
bllyjulian <billyjulian75@gmail.com>
2023-11-24 14:44:36 +0700

    Update API data transaksi

--- api/datatransaksi.php
+++ api/datatransaksi.php
@@ -27,65 +27,105 @@ if (empty($_GET)) {
 
     $result = array();
     while ($row = mysqli_fetch_assoc($query)) {
-        $result[] = array(
-            'id_transaksi' => $row['id_transaksi'],
-            'subtotal' => $row['subtotal'],
-            'koin_dipakai' => $row['koin_dipakai'],
-            'total' => $row['total'],
-            'jenis_status' => $row['jenis_status'],
-            'username' => $row['username'],
-            'judul' => $row['judul'],
-            'harga' => $row['harga'],
-            'nama_pembayaran' => $row['nama_pembayaran'],
-            'icon' => $row['icon'],
-            'tanggal_transaksi' => $row['tanggal_transaksi'],
-            'id_status' => $row['id_status']
-        );
+        $result[] = $row;
     }
     echo json_encode($result);
 } else {
-    // Note: Always use prepared statements for security.
-    $status = mysqli_real_escape_string($connection, $_GET['id_status']);
-    $query = mysqli_query($connection, "SELECT 
-    tb_transaksi.id_transaksi,
-    tb_transaksi.subtotal,
-    tb_transaksi.koin_dipakai,
-    tb_transaksi.total,
-    tb_statustransaksi.jenis_status,
-    tb_transaksi.username,
-    tb_modul.judul,
-    tb_modul.harga,
-    tb_metodepembayaran.nama_pembayaran,
-    tb_metodepembayaran.icon,
-    tb_transaksi.tanggal_transaksi,
-    tb_transaksi.id_status
-FROM 
-    tb_transaksi
-JOIN 
-    tb_statustransaksi ON tb_transaksi.id_status = tb_statustransaksi.id_status
-JOIN 
-    tb_modul ON tb_transaksi.id_modul = tb_modul.id_modul
-JOIN 
-    tb_metodepembayaran ON tb_transaksi.id_pembayaran = tb_metodepembayaran.id_pembayaran WHERE tb_transaksi.id_status='$status'");
+    if (isset($_GET['username']) && isset($_GET['id_status'])) {
+        $username = mysqli_real_escape_string($connection, $_GET['username']);
+        $status = mysqli_real_escape_string($connection, $_GET['id_status']);
 
-    $result = array();
-    while ($row = mysqli_fetch_assoc($query)) {
-        $result[] = array(
-            'id_transaksi' => $row['id_transaksi'],
-            'subtotal' => $row['subtotal'],
-            'koin_dipakai' => $row['koin_dipakai'],
-            'total' => $row['total'],
-            'jenis_status' => $row['jenis_status'],
-            'username' => $row['username'],
-            'judul' => $row['judul'],
-            'harga' => $row['harga'],
-            'nama_pembayaran' => $row['nama_pembayaran'],
-            'icon' => $row['icon'],
-            'tanggal_transaksi' => $row['tanggal_transaksi'],
-            'id_status' => $row['id_status']
-        );
+        $query = mysqli_query($connection, "SELECT 
+            tb_transaksi.id_transaksi,
+            tb_transaksi.subtotal,
+            tb_transaksi.koin_dipakai,
+            tb_transaksi.total,
+            tb_statustransaksi.jenis_status,
+            tb_transaksi.username,
+            tb_modul.judul,
+            tb_modul.harga,
+            tb_metodepembayaran.nama_pembayaran,
+            tb_metodepembayaran.icon,
+            tb_transaksi.tanggal_transaksi,
+            tb_transaksi.id_status
+        FROM 
+            tb_transaksi
+        JOIN 
+            tb_statustransaksi ON tb_transaksi.id_status = tb_statustransaksi.id_status
+        JOIN 
+            tb_modul ON tb_transaksi.id_modul = tb_modul.id_modul
+        JOIN 
+            tb_metodepembayaran ON tb_transaksi.id_pembayaran = tb_metodepembayaran.id_pembayaran 
+        WHERE tb_transaksi.username='$username' AND tb_transaksi.id_status='$status'");
+
+        $result = array();
+        while ($row = mysqli_fetch_assoc($query)) {
+            $result[] = $row;
+        }
+        echo json_encode($result);
+    } elseif (isset($_GET['username'])) {
+        $username = mysqli_real_escape_string($connection, $_GET['username']);
+        $query = mysqli_query($connection, "SELECT 
+            tb_transaksi.id_transaksi,
+            tb_transaksi.subtotal,
+            tb_transaksi.koin_dipakai,
+            tb_transaksi.total,
+            tb_statustransaksi.jenis_status,
+            tb_transaksi.username,
+            tb_modul.judul,
+            tb_modul.harga,
+            tb_metodepembayaran.nama_pembayaran,
+            tb_metodepembayaran.icon,
+            tb_transaksi.tanggal_transaksi,
+            tb_transaksi.id_status
+        FROM 
+            tb_transaksi
+        JOIN 
+            tb_statustransaksi ON tb_transaksi.id_status = tb_statustransaksi.id_status
+        JOIN 
+            tb_modul ON tb_transaksi.id_modul = tb_modul.id_modul
+        JOIN 
+            tb_metodepembayaran ON tb_transaksi.id_pembayaran = tb_metodepembayaran.id_pembayaran 
+        WHERE tb_transaksi.username='$username'");
+
+        $result = array();
+        while ($row = mysqli_fetch_assoc($query)) {
+            $result[] = $row;
+        }
+        echo json_encode($result);
+    } elseif (isset($_GET['id_status'])) {
+        $status = mysqli_real_escape_string($connection, $_GET['id_status']);
+        $query = mysqli_query($connection, "SELECT 
+            tb_transaksi.id_transaksi,
+            tb_transaksi.subtotal,
+            tb_transaksi.koin_dipakai,
+            tb_transaksi.total,
+            tb_statustransaksi.jenis_status,
+            tb_transaksi.username,
+            tb_modul.judul,
+            tb_modul.harga,
+            tb_metodepembayaran.nama_pembayaran,
+            tb_metodepembayaran.icon,
+            tb_transaksi.tanggal_transaksi,
+            tb_transaksi.id_status
+        FROM 
+            tb_transaksi
+        JOIN 
+            tb_statustransaksi ON tb_transaksi.id_status = tb_statustransaksi.id_status
+        JOIN 
+            tb_modul ON tb_transaksi.id_modul = tb_modul.id_modul
+        JOIN 
+            tb_metodepembayaran ON tb_transaksi.id_pembayaran = tb_metodepembayaran.id_pembayaran 
+        WHERE tb_transaksi.id_status='$status'");
+
+        $result = array();
+        while ($row = mysqli_fetch_assoc($query)) {
+            $result[] = $row;
+        }
+        echo json_encode($result);
+    } else {
+        echo json_encode(['error' => 'Invalid parameters']);
     }
-    echo json_encode($result);
 }
 
 mysqli_close($connection);
