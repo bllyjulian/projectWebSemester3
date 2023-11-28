e6083a2c3a0a745aa66fa3233234a49e263a4661
bllyjulian <billyjulian75@gmail.com>
2023-11-28 20:56:18 +0700

    UPDATE API datamodul, data challenge

--- api/datachallenge.php
+++ api/datachallenge.php
@@ -101,6 +101,34 @@ if (empty($_GET)) {
             $result[] = $row;
         }
         echo json_encode($result);
+    } elseif (isset($_GET['username'])) {
+        $username = mysqli_real_escape_string($connection, $_GET['username']);
+
+        $query = mysqli_query($connection, "SELECT 
+            c.id_challenge,
+            c.soal,
+            c.tropi,
+            c.koin,
+            c.kuota,
+            c.id_jenis,
+            c.id_lvlchallenge,
+            lc.jenis_lvl,
+            jc.nama_jenis
+        FROM 
+            tb_challenge c
+        LEFT JOIN 
+            tb_lvlchallenge lc ON c.id_lvlchallenge = lc.id_lvlchallenge
+        LEFT JOIN 
+            tb_jenischallenge jc ON c.id_jenis = jc.id_jenis
+        WHERE c.id_challenge NOT IN (
+            SELECT id_challenge FROM tb_submitchallenge WHERE username = '$username'
+        )");
+
+        $result = array();
+        while ($row = mysqli_fetch_assoc($query)) {
+            $result[] = $row;
+        }
+        echo json_encode($result);
     } else {
         echo json_encode(['error' => 'Invalid parameters']);
     }
---
+++ api/datamodulV2.php
@@ -0,0 +1,46 @@
+<?php
+require_once('connection.php');
+header('Content-Type: application/json;charset=utf8');
+
+if (empty($_GET)) {
+  $query = mysqli_query($connection, "SELECT * FROM tb_modul");
+
+  $result = array();
+  while ($row = mysqli_fetch_assoc($query)) {
+    $result[] = array(
+      'id_modul' => $row['id_modul'],
+      'judul' => $row['judul'],
+      'keterangan' => $row['keterangan'],
+      'gambar' => $row['gambar'],
+      'tujuan' => $row['tujuan'],
+      'harga' => $row['harga'],
+      'id_jenismodul' => $row['id_jenismodul']
+    );
+  }
+  echo json_encode($result);
+} else {
+  $username = mysqli_real_escape_string($connection, $_GET['username']);
+
+  $query = mysqli_query($connection, "SELECT * FROM tb_modul 
+    WHERE id_modul NOT IN (
+      SELECT id_modul FROM tb_moduldimiliki WHERE username = '$username'
+    )");
+
+  $result = array();
+  while ($row = mysqli_fetch_assoc($query)) {
+    $result[] = array(
+      'id_modul' => $row['id_modul'],
+      'judul' => $row['judul'],
+      'keterangan' => $row['keterangan'],
+      'gambar' => $row['gambar'],
+      'tujuan' => $row['tujuan'],
+      'harga' => $row['harga'],
+      'id_jenismodul' => $row['id_jenismodul']
+    );
+  }
+  echo json_encode($result);
+}
+
+mysqli_close($connection);
+
+?>
